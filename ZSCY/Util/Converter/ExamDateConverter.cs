e8ca560f51b0d966651adda5dd6f099264958d1c
杨宇星 <yyx290799684@hotmail.com>
2016-02-01 11:15:38 +0800

    考试安排移植_服务器跪了，待测试

---
+++ ZSCY/Util/Converter/ExamDateConverter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Xaml.Data;
+
+namespace ZSCY.Util.Converter
+{
+    public class ExamDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            ApplicationDataContainer appSetting = Windows.Storage.ApplicationData.Current.LocalSettings;
+            string data = value.ToString();
+            string[] sp = new string[] { "\r" };
+            string[] date = data.Split(sp, StringSplitOptions.None);
+            if (!date[0].Contains("周0"))
+            {
+                Regex r = new Regex(@"\d+");
+                var w = r.Matches(date[0]);
+                int week = Int32.Parse(w[0].Value);
+                if (week > 99)
+                {
+                    return week / 10000 + "\r" + week % 10000 / 100 + "/" + week % 10;
+                }
+                int nowweek = Int32.Parse(appSetting.Values["nowWeek"].ToString());
+                DateTime now = DateTime.Now;
+                int dayofweek = (int)now.DayOfWeek == 0 ? 7 : (int)now.DayOfWeek;
+                DateTime targetdate = now.AddDays((week - nowweek) * 7 + Int32.Parse(w[1].Value) - dayofweek);
+                string weekname = date[0].Substring(date[0].Length - 2, 2);
+                string replaceweekname = "";
+                switch (weekname)
+                {
+                    case "周1": replaceweekname = "周一"; break;
+                    case "周2": replaceweekname = "周二"; break;
+                    case "周3": replaceweekname = "周三"; break;
+                    case "周4": replaceweekname = "周四"; break;
+                    case "周5": replaceweekname = "周五"; break;
+                    case "周6": replaceweekname = "周六"; break;
+                    case "周7": replaceweekname = "周日"; break;
+                }
+                string result = date[0].Replace(weekname, replaceweekname);
+                return result + "\r" + targetdate.Month + "/" + targetdate.Day;
+            }
+            return "待\r定";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ZSCY/Util/Converter/StarFillConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Xaml.Data;
+
+namespace ZSCY.Util.Converter
+{
+    public class StarFillConverter : IValueConverter
+    {
+        ApplicationDataContainer appSetting = Windows.Storage.ApplicationData.Current.LocalSettings;
+
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            ApplicationDataContainer appSetting = Windows.Storage.ApplicationData.Current.LocalSettings;
+            string data = value.ToString();
+            string[] sp = new string[] { "\r" };
+            string[] date = data.Split(sp, StringSplitOptions.None);
+            if (!date[0].Contains("周0"))
+            {
+                Regex r = new Regex(@"\d+");
+                var w = r.Matches(date[0]);
+                int week = Int32.Parse(w[0].Value);
+                int nowweek = Int32.Parse(appSetting.Values["nowWeek"].ToString());
+                DateTime now = DateTime.Now;
+                int dayofweek = (int)now.DayOfWeek == 0 ? 7 : (int)now.DayOfWeek;
+                if (nowweek == week && dayofweek > Int32.Parse(w[1].Value))
+                {
+                    return "/Assets/StarFilled.png";
+                }
+                else if (nowweek > week)
+                {
+                    return "/Assets/StarFilled.png";
+                }
+                else
+                {
+                    return "/Assets/StarEmpty.png";
+                }
+            }
+            return "/Assets/starun.png";
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
