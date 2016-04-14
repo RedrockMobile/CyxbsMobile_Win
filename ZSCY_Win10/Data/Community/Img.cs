99948ff7b2511d1d1d854f9c08adc7da8b75666c
Lin Jie <ljlovemichael@hotmail.com>
2016-04-14 17:35:31 +0800

    给类添加Json解析方法

--- ZSCY_Win10/Data/Community/Img.cs
+++ ZSCY_Win10/Data/Community/Img.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace ZSCY_Win10.Data.Community
 {
@@ -10,5 +11,12 @@ namespace ZSCY_Win10.Data.Community
     {
         public string ImgId { get; set; }
         public string ImgSmallSrc { get; set; }
+
+        public Img GetAttributes(JObject imgJObjcet)
+        {
+            ImgId = imgJObjcet["img_id"].ToString();
+            ImgSmallSrc = imgJObjcet["img_small_src"].ToString();
+            return this;
+        }
     }
 }
---
+++ ZSCY_Win10/Service/CommunityFeedsService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZSCY_Win10.Data.Community;
+
+namespace ZSCY_Win10.Service
+{
+    public class CommunityFeedsService
+    {
+
+        const string hotFeeds = @"cyxbsMobile/index.php/Home/Article/searchHotArticle";
+        /// <summary>
+        /// 获取动态列表
+        /// </summary>
+        /// <param name="type">动态参数，cyxw=>1,jwzx=>2,xsjz=>3,xwgg=>4,bbdd=>5</param>
+        /// <returns>返回参数对应的列表数据</returns>
+        public static List<Feeds> GetDatas(int type,int page,int size)
+        {
+            List<KeyValuePair<String, String>> paramList = new List<KeyValuePair<String, String>>();
+
+            return null;
+        }
+    }
+}
