9240c31939cc195b8af5cad5f1008d2d7206c1a6
Lin Jie <ljlovemichael@hotmail.com>
2016-04-15 02:44:58 +0800

    逼逼叨叨动态页面基本完成，可以看到东西了。

--- ZSCY_Win10/Service/CommunityFeedsService.cs
+++ ZSCY_Win10/Service/CommunityFeedsService.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Data.Json;
+using Newtonsoft.Json.Linq;
 using ZSCY_Win10.Data.Community;
+using ZSCY_Win10.Util;
 
 namespace ZSCY_Win10.Service
 {
@@ -16,10 +20,39 @@ namespace ZSCY_Win10.Service
         /// </summary>
         /// <param name="type">动态参数，cyxw=>1,jwzx=>2,xsjz=>3,xwgg=>4,bbdd=>5</param>
         /// <returns>返回参数对应的列表数据</returns>
-        public static List<Feeds> GetDatas(int type,int page,int size)
+        public static async Task<List<Feeds>> GetDatas(int type = 5, int page = 1, int size = 1)
         {
             List<KeyValuePair<String, String>> paramList = new List<KeyValuePair<String, String>>();
+            paramList.Add(new KeyValuePair<string, string>("stuNum", "2013211429"));
+            paramList.Add(new KeyValuePair<string, string>("idNum", "252617"));
+            paramList.Add(new KeyValuePair<string, string>("type", type.ToString()));
+            paramList.Add(new KeyValuePair<string, string>("page", page.ToString()));
+            paramList.Add(new KeyValuePair<string, string>("size", size.ToString()));
+            string response = await NetWork.getHttpWebRequest(hotFeeds, paramList);
+            try
+            {
+                JArray jsonstr = JArray.Parse(response);
 
+                List<Feeds> feedslist = new List<Feeds>();
+
+                for (int j = 0; j < jsonstr.Count; j++)
+                {
+                    JObject jsondetail = (JObject)(jsonstr[j]);
+                    if (jsondetail["status"].ToString() == "200")
+                    {
+                        JObject jo = (JObject)jsondetail["data"];
+                        Feeds f = new Feeds();
+                        f.GetAttributes(jo);
+                        feedslist.Add(f);
+                    }
+                }
+                return feedslist;
+
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
             return null;
         }
     }
