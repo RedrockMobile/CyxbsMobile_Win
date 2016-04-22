ef4faf0085b1e022dd6fbacbd3df36c5a308f93c
Lin Jie <ljlovemichael@hotmail.com>
2016-04-23 01:03:59 +0800

    加入推送通知，可以在Azure发通知啦。

---
+++ ZSCY_Win10/Data/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZSCY_Win10.Data
+{
+    public class TodoItem
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public bool Complete { get; set; }
+    }
+}
