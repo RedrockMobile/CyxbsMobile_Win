3402ddae4737d766c890554cdfca17cdb55dc5b5
Lin Jie <ljlovemichael@hotmail.com>
2016-05-02 18:31:36 +0800

    我的消息类

---
+++ ZSCY_Win10/Data/Community/MyNotification.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace ZSCY_Win10.Data.Community
+{
+    public class MyNotification:IFeeds
+    {
+
+
+        public string type { get; set; }
+        public string content { get; set; }//回复内容
+        public string article_content { get; set; }//原文
+        public Img[] article_photo_src { get; set; }
+        public string created_time { get; set; }
+        public string article_id { get; set; }
+        public string stunum { get; set; }
+        public string nickname { get; set; }
+        public string photo_src { get; set; }
+
+
+        public void GetAttributes(JObject feedsJObject)
+        {
+            type = feedsJObject["type"].ToString();
+            content = feedsJObject["content"].ToString();
+            article_content = feedsJObject["article_content"].ToString();
+            created_time = feedsJObject["created_time"].ToString();
+            article_id = feedsJObject["article_id"].ToString();
+            stunum = feedsJObject["stunum"].ToString();
+            nickname = feedsJObject["nickname"].ToString();
+            photo_src = feedsJObject["photo_src"].ToString();
+            string articlephotos = feedsJObject["article_photo_src"].ToString();
+            string picstart = "http://hongyan.cqupt.edu.cn/cyxbsMobile/Public/photo/";
+            if (articlephotos != "")
+            {
+                try
+                {
+                    string[] i = articlephotos.Split(new char[] { ',' }, 9);
+                    if (i.Length > 1)
+                        article_photo_src = new Img[i.Length - 1];
+                    else
+                        article_photo_src = new Img[i.Length];
+                    for (int j = 0; j < article_photo_src.Length; j++)
+                    {
+                        if (i[j] != "")
+                        {
+                            if (!i[j].StartsWith(picstart))
+                                i[j] = picstart + i[j];
+                            article_photo_src[j] = new Img();
+                            article_photo_src[j].ImgSrc = i[j];
+                            article_photo_src[j].ImgSmallSrc = i[j];
+                        }
+                        else
+                        {
+                            article_photo_src[j] = new Img();
+                            article_photo_src[j].ImgSrc = article_photo_src[j].ImgSmallSrc = "";
+
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e.Message);
+
+                }
+            }
+        }
+    }
+}
