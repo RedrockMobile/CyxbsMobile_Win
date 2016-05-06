7e9657fe266db89f36dd5cfab8fb3db58d4d22ff
Lin Jie <ljlovemichael@hotmail.com>
2016-05-06 10:44:58 +0800

    我的消息完善，加入我的动态中

---
+++ ZSCY_Win10/ViewModels/Community/MyViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZSCY_Win10.Data.Community;
+using ZSCY_Win10.Service;
+
+namespace ZSCY_Win10.ViewModels.Community
+{
+    public class MyViewModel
+    {
+        public ObservableCollection<MyNotification> MyNotify { get; private set; } = new ObservableCollection<MyNotification>();
+        public ObservableCollection<MyFeed> MyFeedlist { get; private set; } = new ObservableCollection<MyFeed>();
+        public  MyViewModel()
+        {
+            Get();
+        }
+        private async Task Get()
+        {
+            List<MyNotification> s = await MyService.GetNotifications(0, 15);
+            for (int i = 0; i < s.Count; i++)
+            {
+                MyNotify.Add(s[i]);
+            }
+            List<MyFeed> f = await MyService.GetMyFeeds(0, 15);
+            for (int i = 0; i < f.Count; i++)
+            {
+                MyFeedlist.Add(f[i]);
+            }
+        }
+    }
+}
