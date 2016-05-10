d6969b6ce234e518d37be1999c4764a36ba3efc0
杨宇星 <yyx290799684@hotmail.com>
2016-05-10 13:13:23 +0800

    无头像的崩溃问题

--- ZSCY_Win10/Data/Community/PeoInfo.cs
+++ ZSCY_Win10/Data/Community/PeoInfo.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
+using Windows.Storage;
 
 namespace ZSCY_Win10.Data.Community
 {
-    public class PeoInfo:IFeeds
+    public class PeoInfo : IFeeds
     {
-
+        ApplicationDataContainer appSetting = Windows.Storage.ApplicationData.Current.LocalSettings;
         public PeoInfo() { }
         public PeoInfo(string nickname, string introduction, string phone, string qq)
         {
@@ -37,10 +38,14 @@ namespace ZSCY_Win10.Data.Community
             stunum = feedsJObject["stunum"].ToString();
             introduction = feedsJObject["introduction"].ToString();
             username = feedsJObject["username"].ToString();
+
             nickname = feedsJObject["nickname"].ToString();
+            //}
             gender = feedsJObject["gender"].ToString();
-            photo_thumbnail_src = feedsJObject["photo_thumbnail_src"].ToString();
-            photo_src = feedsJObject["photo_src"].ToString();
+            //photo_thumbnail_src = feedsJObject["photo_thumbnail_src"].ToString();
+            //photo_src = feedsJObject["photo_src"].ToString();
+            photo_thumbnail_src = feedsJObject["photo_thumbnail_src"].ToString() == "" ? "ms-appx:///Assets/Community_nohead.png" : feedsJObject["photo_thumbnail_src"].ToString();
+            photo_src = feedsJObject["photo_src"].ToString() == "" ? "ms-appx:///Assets/Community_nohead.png" : feedsJObject["photo_src"].ToString();
             updated_time = feedsJObject["updated_time"].ToString();
             phone = feedsJObject["phone"].ToString();
             qq = feedsJObject["qq"].ToString();
